9e01feaab98b7bc2faef0fe07a690fb7fa4ad85b
Otto Lambe <z5161264@ad.unsw.edu.au>
2022-07-04 10:59:54 +1000

    Changed SSR to false

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,7 @@ import 'tailwindcss/tailwind.css';
 import '../configureAmplify';
 import awsconfig from '../aws-exports';
 import Amplify from 'aws-amplify';
-Amplify.configure({...awsconfig, ssr: true});
-
+Amplify.configure({...awsconfig, ssr: false});
 
 function MyApp({ Component, pageProps }) {
   return (

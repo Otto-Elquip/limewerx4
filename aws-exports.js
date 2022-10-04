5f0b132959c970804f0f5f9ac41020e2fb172c77
Otto Lambe <z5161264@ad.unsw.edu.au>
2022-10-05 08:59:28 +1100

    Added new API key

--- aws-exports.js
+++ aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://nw66crvzmzehho3sdw4lxv3l34.appsync-api.ap-southeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-3ifxibki6vekbjboiashqu7wpm",
+    "aws_appsync_apiKey": "da2-xghgsnuetjcsvnymff2vge4cay",
     "aws_cognito_identity_pool_id": "ap-southeast-2:182d069b-3298-47c0-a476-4ec6fa7b4250",
     "aws_cognito_region": "ap-southeast-2",
     "aws_user_pools_id": "ap-southeast-2_dhpx2oMeM",
